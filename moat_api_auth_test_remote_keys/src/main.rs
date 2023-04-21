72d333210584b7bc4b5cce99554f44c2cffeb123
Attila Molnár <wd977002@varianm365.com>
2023-04-21 15:41:21 +0200

    refactor

--- moat_api_auth_test_remote_keys/src/main.rs
+++ moat_api_auth_test_remote_keys/src/main.rs
@@ -12,9 +12,7 @@ use moat_tool_jwt_handler::features::{
     services::{
         key_handlers::{key_handler::PublicKeyHandlerSafe, remote_key_handler::RemoteKeyHandler},
         token_validation::{
-            basic_token_validator::BasicTokenValidator,
-            remote_url_token_validator::RemoteUrlTokenValidator,
-            token_validator::TokenValidatorSafe,
+            basic_token_validator::BasicTokenValidator, token_validator::TokenValidatorSafe,
         },
     },
 };
@@ -25,18 +23,17 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    // let key_handler = Arc::new(RemoteKeyHandler::init("./keys.json"));
-    // let token_validator: Arc<TokenValidatorSafe> =
-    //     Arc::new(BasicTokenValidator::init(key_handler.clone()));
-
     let http_client = Arc::new(reqwest::Client::builder().build().unwrap());
-    let key_handler =
-        RemoteKeyHandler::init("http://localhost:8080/auth/jwk", http_client.clone()).await;
-    let key_handler: Arc<PublicKeyHandlerSafe> = Arc::new(key_handler.clone());
+    let key_handler: Arc<PublicKeyHandlerSafe> = Arc::new(
+        RemoteKeyHandler::init2(get_jwk_response(
+            "http://localhost:8080/auth/jwk",
+            http_client.clone(),
+        ))
+        .await,
+    );
 
-    let token_validator = BasicTokenValidator::init(key_handler.clone());
-
-    let token_validator: Arc<TokenValidatorSafe> = Arc::new(token_validator.clone());
+    let token_validator: Arc<TokenValidatorSafe> =
+        Arc::new(BasicTokenValidator::init(key_handler.clone()));
 
     HttpServer::new(move || {
         App::new()
@@ -57,4 +54,12 @@ async fn main() -> std::io::Result<()> {
 #[has_any_permission["User"]]
 async fn get_protected() -> HttpResponse {
     HttpResponse::Ok().body("You can see this protected content that was validated by the centralized auth service. Try again with invalid token.")
+}
+
+async fn get_jwk_response(url: &str, http_client: Arc<reqwest::Client>) -> Result<String, String> {
+    let result = http_client.get(url).send().await;
+    match result {
+        Ok(r) => Ok(r.text().await.unwrap()),
+        Err(err) => Err(format!("{}", err)),
+    }
 }
